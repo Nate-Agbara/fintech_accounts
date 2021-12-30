4081f3c91afdbed99e3b86f5516c20784fa78f3c
Nate-Agbara <Agbaraabah1>
2021-12-30 22:31:30 +0100

    added a test case

--- src/test/java/com/fintech/accounts/AccountsApplicationTests.java
+++ src/test/java/com/fintech/accounts/AccountsApplicationTests.java
@@ -1,25 +1,12 @@
 package com.fintech.accounts;
 
-import com.fintech.accounts.model.Account;
-import com.fintech.accounts.model.AccountStatus;
-import com.fintech.accounts.repository.AccountsRepository;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.math.BigDecimal;
-import java.util.Optional;
-import java.util.Random;
-
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
